b54cfb452c8ae0f059d80ca07cf6eeadb696e9a9
Stephen Maurer <stephen.maurer@icloud.com>
2023-03-25 21:53:18 +0000

    Added local copy of libraires for offline usage of this code

--- ptz.php
+++ ptz.php
@@ -5,8 +5,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>PTZ Control</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-icons.css">
 
 	<style>
 		#direction-pad {
@@ -144,7 +144,8 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<script>
-	var connectionUrl = 'ws://192.168.1.213:8765';
+
+	var connectionUrl = 'ws://<?= $_SERVER['SERVER_ADDR']; ?>:8765';
     var connection;
     var isConnected = false;
     var retryCount = 0;
